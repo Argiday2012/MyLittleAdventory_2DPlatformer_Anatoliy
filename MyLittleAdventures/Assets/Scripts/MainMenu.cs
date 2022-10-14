65843da4210ed8432a2ab2191c6b3e058c6a9a4d
Argiday2012 <anatoli.sidorenkov0@gmail.com>
2022-10-14 16:44:17 +0300

    CompletedPauseMenu

--- MyLittleAdventures/Assets/Scripts/MainMenu.cs
+++ MyLittleAdventures/Assets/Scripts/MainMenu.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    private GameObject firstCamera;
-    private GameObject secondCamera;
+    //private GameObject firstCamera;
+    //private GameObject secondCamera;
 
-    bool flag = false;
+    //bool flag = false;
 
-    private void Awake()
-    {
-        firstCamera = GameObject.Find("Main Camera");
-        secondCamera = GameObject.Find("UICamera");
-        firstCamera.SetActive(false);
-    }
+    //private void Awake()
+    //{
+    //    firstCamera = GameObject.Find("Main Camera");
+    //    secondCamera = GameObject.Find("UICamera");
+    //    firstCamera.SetActive(false);
+    //}
 
-    private void FixedUpdate()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            flag = !flag;
-            firstCamera.SetActive(flag);
-            secondCamera.SetActive(!flag);
-        }
-    }
+    //private void FixedUpdate()
+    //{
+    //    //if(Input.GetKeyDown(KeyCode.Escape))
+    //    //{
+    //    //    flag = !flag;
+    //    //    firstCamera.SetActive(flag);
+    //    //    secondCamera.SetActive(!flag);
+    //    //}
+    //}
 }
